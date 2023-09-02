1788291fc227f04cefa1483d1f5ba5fcf42c875c
Priyam Gautam <98340870+Priyam117@users.noreply.github.com>
2023-09-02 10:21:21 +0530

    Day 10 Assignment complete

---
+++ Day-10/Readme.md
@@ -0,0 +1,14 @@
+
+# Day 10 Class Assignment
+
+This is a simple HTML, CSS, and JavaScript project that creates a circle that can change color and shape.
+
+## HTML
+
+The HTML code creates a container div with a circle div inside it. The circle div has a shape div inside it. The shape div is initially a circle, but can be changed to a triangle by clicking the "change shape" button.
+
+
+## CSS
+
+The CSS code styles the container div, the circle div, and the shape div. The container div is centered on the page and has a width and height of 500px. The circle div is positioned absolutely inside the container div and has a width and height of 200px. The shape div is positioned absolutely inside the circle div and has a width and height of 100px.
+
---
+++ Day-10/index.js
@@ -0,0 +1,28 @@
+let Color = ["green","cyan","black","voilet","blue","yellow","purple","orange","lightgreen",
+	"lemon",];
+let index = 0;
+const circle = document.getElementById("circle");
+const shape = document.querySelector(".shape");
+const Changecolor = document.querySelector("#color");
+const changeShape = document.querySelector("#changeShape");
+
+Changecolor.addEventListener("click", () => {
+	if (index === Color.length) {
+		index = 0;
+	}
+	circle.style.backgroundColor = Color[index];
+	index++;
+})
+let Tringle = false;
+changeShape.addEventListener("click", () => {
+	if (!Tringle) {
+		let Shape = document.querySelector(".shape");
+		Shape.className = "triangle";
+		Tringle = true;
+	}
+	else {
+		var shape = document.querySelector(".triangle");
+		shape.className = "shape";
+		Tringle = false;
+	}
+})
